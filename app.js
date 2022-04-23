d2679f13204d816bba8260deacfcd7f77aaba1cc
Cheyenne Piper <72780256+PiperRc@users.noreply.github.com>
2022-04-22 23:25:47 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const label=document.querySelectorAll('label');
 const radiobtn=document.querySelectorAll('.radiobtn');
-
 const submit=document.querySelector('.submit-btn');
 const container=document.querySelector('.container');
 const thankyou=document.querySelector('.thankyou');
-const rating=document.querySelector('.selected span')
-// const form=document.querySelector('form');
+const rating=document.querySelector('.selected span');
+const main=document.querySelector('main');
 
-
- 
 const getValue=() => {
-   
-    const radiobtn = document.querySelectorAll('.radiobtn');
+
     for (i = 0; i < radiobtn.length; i++) {
         if (radiobtn[i].checked) {
            return label[i].textContent;
@@ -21,22 +17,16 @@ const getValue=() => {
 }
 
 submit.addEventListener('click',()=>{
-
-container.style.opacity=0;
-container.style.zIndex=-1;
-thankyou.style.opacity=1;
-thankyou.style.zIndex=1;
-rating.innerHTML=getValue();
+for(i=0;i<radiobtn.length;i++){
+    if(radiobtn[i].checked){
+        container.style.opacity=0;
+        container.style.zIndex=-1;
+        thankyou.style.opacity=1;
+        thankyou.style.zIndex=1;
+        rating.innerHTML=getValue();
+        main.style.paddingTop='2rem';
+        main.style.paddingBottom='7rem';
+    } 
+}
 })
 
-
-// // sent.html
-// let charlene=document.querySelector('.charlene')
-// form.addEventListener('submit',(e)=>{
-
-//     e.preventDefault();
-//     charlene.innerHTML=getValue();
-// })
-
-
-// // 
